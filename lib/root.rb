643389868f532d76696fb03b6e8a9a82472a3ad4
ayellapragada <ayellapragada@gmail.com>
2020-03-28 15:28:31 -0400

    Dynamically import everything up front!

--- lib/root.rb
+++ lib/root.rb
@@ -1,60 +1,10 @@
 # frozen_string_literal: true
 
-require 'root/version'
-require 'root/game'
+Dir.glob('lib/**/*.rb').drop(1).each do |file|
+  require file.gsub('lib/', '').gsub('.rb', '')
+end
 
-require 'root/boards/woodlands'
-require 'root/boards/woodlands_generator'
-
-require 'root/cards/base'
-require 'root/cards/item'
-
-require 'root/decks/base'
-require 'root/decks/list'
-require 'root/decks/starter'
-
-require 'root/display/terminal'
-require 'root/display/woodlands_terminal'
-
-require 'root/factions/bird'
-require 'root/factions/cat'
-require 'root/factions/mouse'
-require 'root/factions/vagabond'
-
-require 'root/factions/cats/catable'
-require 'root/factions/cats/keep'
-require 'root/factions/cats/recruiter'
-require 'root/factions/cats/sawmill'
-require 'root/factions/cats/wood'
-require 'root/factions/cats/workshop'
-
-require 'root/factions/birds/birdable'
-require 'root/factions/birds/decree'
-require 'root/factions/birds/leader'
-require 'root/factions/birds/roost'
-
-require 'root/factions/mice/miceable'
-require 'root/factions/mice/base'
-require 'root/factions/mice/sympathy'
-
-require 'root/factions/vagabonds/character'
-require 'root/factions/vagabonds/characters'
-require 'root/factions/vagabonds/quest_card'
-require 'root/factions/vagabonds/quest_deck'
-require 'root/factions/vagabonds/relationships'
-require 'root/factions/vagabonds/vagabondable'
-
-require 'root/grid/clearing'
-require 'root/grid/empty_slot'
-require 'root/grid/ruin'
-
-require 'root/players/computer'
-require 'root/players/human'
-require 'root/players/list'
-
-require 'root/pieces/building'
-require 'root/pieces/token'
-require 'root/pieces/meeple'
-
+# Our big boi Root module.
+# Nothing really goes in here, I guess eventually a game.start will.
 module Root
 end
