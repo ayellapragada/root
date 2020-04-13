a90b43d17af8ceb7e086f378786912911678ade0
ayellapragada <ayellapragada@gmail.com>
2020-04-13 11:05:38 -0400

    Woo! Added a concept of Menus using less + pager
    Nice nice nice this is neat, this gives the app Menus!
    Right now I can only think of 2 - 3 useful ones
    
    a Discarded Cards menu - NEEDED
    a Help menu - NEEDED
    a History menu to see the whole history - NICE TO HAVE
    
    This also adds slightly more useful methods for cursor control.
    Those will need to be refactored to the Cursor object probably.

--- lib/root/display/terminal.rb
+++ lib/root/display/terminal.rb
@@ -8,6 +8,7 @@ module Root
     # This handles all of that sort of logic here.
     class Terminal
       class InputError < StandardError; end
+      class ShowMenu < StandardError; end
 
       # NOT TESTING TERMINAL BECAUSE OOFY
       #:nocov:
@@ -21,36 +22,43 @@ module Root
           input_for_options(options)
         end
       rescue Root::Display::Terminal::InputError
-        puts 'oops'
+        puts 'Invalid Choice, try again'
         retry
       end
 
       def render_game(game, _player_to_view_as, clearings)
-        game_map = Root::Display::WoodlandsMap.new(game.board, clearings).display
+        map = Root::Display::WoodlandsMap.new(game.board, clearings).display
         history = Root::Display::History.new(game.history).display
 
-        merged = game_map.map.with_index do |i, idx|
+        merged = map.map.with_index do |i, idx|
           hist = history[idx] || '' # Default History / Empty Spaces
           i + hist
         end
 
-        current = Cursor.pos
-
-        puts "\e[0;0H"
-
+        current_row = Cursor.pos[:row]
+        move_cursor_to_top
         merged.each { |row| puts row }
+        clear_out_rest_of_screen(current_row, map.length)
+      end
 
+      def clear_out_rest_of_screen(current_row, total_rows)
         puts "\e[s"
-        remaining = current[:row] - game_map.length
+        remaining = current_row - total_rows
         remaining.times do
           puts "\e[0K"
         end
         puts "\e[u"
       end
 
+      def move_cursor_to_top
+        puts "\e[0;0H"
+      end
+
       private
 
       def display_pick_option_message(key)
+        puts 'Use "?" for help and "discard" to check discard pile.'
+        puts ''
         puts Messages::LIST[key][:prompt]
       end
 
@@ -76,19 +84,57 @@ module Root
       end
 
       def get_and_map_choice_to_index(options)
-        priority = gets.chomp
+        option = handle_getting_input
+
         options.find_index do |o|
-          o.priority == priority.to_i ||
-            o.priority == priority.upcase.to_sym
+          o.priority == option.to_i ||
+            o.priority == option.upcase.to_sym
         end.tap do |res|
           raise InputError unless res
         end
       end
 
       def input_for_options(options)
-        (gets.chomp.to_i - 1).tap do |res|
+        option = handle_getting_input
+        (option.to_i - 1).tap do |res|
           raise InputError if res <= -1 || res >= options.count
         end
+      end
+
+      def handle_getting_input
+        menu_opts = %w[? discard]
+        loop do
+          option = gets.chomp
+          return option unless menu_opts.include?(option)
+
+          handle_showing_menu(option)
+          clear_previous_line
+        end
+      end
+
+      def clear_previous_line
+        puts "\e[2A"
+        puts "\e[0K"
+        puts "\e[2A"
+      end
+
+      def handle_showing_menu(option)
+        render_help if option == '?'
+        render_discard if option == 'discard'
+      end
+
+      def render_help
+        very_long_string1 = Array.new(50) { 'this is help' }
+        very_long_string2 = Array.new(50) { 'this is also help' }
+        very_long_string = (very_long_string1 + very_long_string2).join("\n")
+        IO.popen('less', 'w') { |f| f.puts very_long_string }
+      end
+
+      def render_discard
+        very_long_string1 = Array.new(50) { 'discardabba' }
+        very_long_string2 = Array.new(50) { 'bbbbbbbbbdiscard' }
+        very_long_string = (very_long_string1 + very_long_string2).join("\n")
+        IO.popen('less', 'w') { |f| f.puts very_long_string }
       end
       #:nocov:
     end
