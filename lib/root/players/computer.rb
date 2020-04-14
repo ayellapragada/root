1616eefd6d95fddc8725ad7f0975a974a3324491
ayellapragada <ayellapragada@gmail.com>
2020-04-14 13:26:51 -0400

    Game is getting more playable
    Display is usable, missing things like Victory Points and Items left,
    but not MVP atm.
    
    1. Next step is quick Mouse render on board, don't even need a turn,
       just the quickest rendering to get back to one hundo.
    2. Vagabondo rename
    3. R E F A C T O R
        a. All my classes are too big LOL

--- lib/root/players/computer.rb
+++ lib/root/players/computer.rb
@@ -13,6 +13,15 @@ module Root
         options.find_index(choice)
       end
 
+      # Easier "SMART" mode:
+      # choice = Computer::Intelligence.make_move(key, opts, @game)
+      # Maybe don't even necessarily do the CWE. Just do the bots.
+      # Don't even necessarily have to be _great_ but playable would be neat,
+      # lots of common major logic workflows,
+      # like when to do thing or not do things
+      # after that, simple enough to use some common CWE tricks,
+      # such as clearing priorities.
+
       def render_game(*)
         # Surprise we don't render anything for a computer :wow:
       end
