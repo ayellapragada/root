5d3b158582aa961e3e6e9f99d1e52d7b04f37287
ayellapragada <ayellapragada@gmail.com>
2020-05-09 13:40:16 -0400

    Add test for racoon power

--- spec/root/factions/racoons/characters/character_spec.rb
+++ spec/root/factions/racoons/characters/character_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Root::Factions::Racoons::Characters::Thief do
+RSpec.describe Root::Factions::Racoons::Characters::Character do
   let(:player) { Root::Players::Computer.for('Sneak', :racoon) }
   let(:faction) { player.faction }
   let(:char) { faction.character }
@@ -13,6 +13,13 @@ RSpec.describe Root::Factions::Racoons::Characters::Thief do
       char.f.damage_item(:torch)
 
       expect(char.torch?).to be false
+    end
+  end
+
+  describe '#racoon.special_name' do
+    it 'delegates to character and gets name' do
+      faction.quick_set_character(:thief)
+      expect(faction.special_name).to eq(:steal)
     end
   end
 end
