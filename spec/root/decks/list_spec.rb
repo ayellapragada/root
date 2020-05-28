7e5aa0f9c1a8bead62b405a5dc99f31624bf8798
ayellapragada <ayellapragada@gmail.com>
2020-05-28 19:12:11 -0400

    Add in test for deck.from_db

--- spec/root/decks/list_spec.rb
+++ spec/root/decks/list_spec.rb
@@ -1,19 +1,30 @@
 # frozen_string_literal: true
+
 RSpec.describe Root::Decks::List do
   describe '.from_db' do
-    xit 'sets the values of all decks' do
+    it 'sets the values of all decks' do
       db_record = {
-        shared: %i[tea tea sword],
-        discard: %i[cats cats birds mice],
-        lost_souls: %i[sawmill wood cats],
-        dominance: %i[], # TODO: WILL COME BACK TO LATER
-        quests: %i[base sympathy],
-        active_quests: %i[ruin sword],
+        shared: [{ name: 'Cobbler', suit: :rabbit }],
+        discard: [
+          { name: 'Ambush', suit: :bird },
+          { name: "Smuggler's Trail", suit: :rabbit }
+        ],
+        lost_souls: [{ name: 'Anvil', suit: :fox }],
+        dominance: [{ name: 'Dominance', suit: :bird }],
+        quests: [{ name: 'Fundraising', suit: :fox }],
+        active_quests: [{ name: 'Errand', suit: :fox }],
         characters: %i[ranger tinker]
       }
 
       decks = Root::Decks::List.from_db(db_record)
-      expect(decks).to be_truthy
+      expect(decks.shared.dominance_for(:bird)).to be_truthy
+      expect(decks.shared.dominance_for(:fox)).to be nil
+      expect(decks.shared.deck.count).to be(1)
+      expect(decks.shared.discard.count).to be(2)
+      expect(decks.shared.lost_souls.count).to be(1)
+      expect(decks.quests.deck.count).to be(1)
+      expect(decks.quests.active_quests.count).to be(1)
+      expect(decks.characters.map(&:type)).to eq(%i[ranger tinker])
     end
   end
 end
